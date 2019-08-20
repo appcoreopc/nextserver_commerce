faf231d6b8344bc178eeac357dacc909aed9037f
jeremy woo <kepungmoin@hotmail.com>
2019-08-20 17:46:59 +1200

    updated packages and added contact us page

---
+++ pages/contactus/index.tsx
@@ -0,0 +1,9 @@
+
+const ContactUs = () => (
+
+  <div> 
+      <h1>Contact Us  </h1>
+  </div>
+);
+
+export default ContactUs;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import Link from 'next/link'
+
 
 const Home = () => (
   
   <div>
 
-<script type="text/javascript" src="/static/test.js"></script>
+  <script type="text/javascript" src="/static/test.js"></script>
+
+  <Link href="http://www.google.com"> Hello</Link>
 
   <h1>Hello world!  </h1>
   </div>
