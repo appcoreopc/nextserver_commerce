7fb9bb3dacc9aa66a58cfc69efa2c3c527b1d59f
jeremy woo <kepungmoin@hotmail.com>
2019-08-26 20:45:22 +1200

    fix top nav class name

--- components/TopNavigation/TopNav.jsx
+++ components/TopNavigation/TopNav.jsx
@@ -2,17 +2,17 @@
 export default class TopNav extends React.Component {
     render() {
       return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">ShoeShop</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">ShoeShop</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-            <a class="nav-item nav-link" href="#">Men</a>
-            <a class="nav-item nav-link" href="#">Women</a>
-            <a class="nav-item nav-link disabled" href="#" tabindex="-1">Children</a>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a className="nav-item nav-link" href="#">Men</a>
+            <a className="nav-item nav-link" href="#">Women</a>
+            <a className="nav-item nav-link disabled" href="#" tabindex="-1">Children</a>
           </div>
         </div>
       </nav>
