b63b7e4ecad7900c39b31806869b0ac5f317eadf
jeremy woo <kepungmoin@hotmail.com>
2019-08-28 17:02:29 +1200

    added sw

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 const withCSS = require('@zeit/next-css')
+const NextWorkboxWebpackPlugin = require('next-workbox-webpack-plugin');
+
 
 module.exports = withBundleAnalyzer({
   analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
@@ -19,3 +21,26 @@ module.exports = withBundleAnalyzer({
 module.exports = withCSS({
   cssModules: true
 })
+
+module.exports = {
+  webpack: (config, {isServer, dev, buildId, config: {distDir}}) => {
+    if (!isServer && !dev) {
+      config.plugins.push(
+        new NextWorkboxWebpackPlugin({
+          importWorkboxFrom: 'cdn',
+          distDir,
+          buildId,
+          swDestRoot: './static/workbox',
+          swURLRoot: '/static/js'
+        })
+      );
+    }
+
+    return config
+  },
+  exportPathMap: () => {
+    return {
+      '/': { page: '/' }
+    }
+  }
+}
